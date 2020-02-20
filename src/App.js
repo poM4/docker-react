206e145bd964295b75e35dd2bd38f398daa4614f
poM4 <romaigoroleg@gmail.com>
2020-02-20 08:55:13 +0200

    change text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Knock Knock Neo. Follow White Rabbit. Kiss
+          Knock Knock Neo. Follow The White Rabbit.
         </p>
       </div>
     );
